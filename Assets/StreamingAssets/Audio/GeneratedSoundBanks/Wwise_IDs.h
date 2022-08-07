1559f1a876fc906c43552f7eb3bd19a02b155e84
VeronicaBazylchyk <109976317+VeronicaBazylchyk@users.noreply.github.com>
2022-08-07 13:46:02 +0300

    Добавление треков(2) и залив SoundBank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CAT = 983016379U;
+        static const AkUniqueID DOOR = 1877847629U;
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID GOOD_NIGHT_CAT = 2518459408U;
+        static const AkUniqueID STERTGAME = 607282865U;
+        static const AkUniqueID WAY_TO_HOME = 1003831224U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GROUNDTYPE
+        {
+            static const AkUniqueID GROUP = 2835351336U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID STEP_STAR = 3835575458U;
+                static const AkUniqueID STEP_WOOD = 1041114565U;
+            } // namespace SWITCH
+        } // namespace GROUNDTYPE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIET = 3163593783U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID PLAYER = 1069431850U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
